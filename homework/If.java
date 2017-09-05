a15934d72fbcc23f59c341e4b3f457ce7fb6c6e6
GavinDes <gavin17.business@gmail.com>
2017-09-05 18:09:48 -0500

    Added blank homework scripts

---
+++ homework/If.java
@@ -0,0 +1,22 @@
+/* 
+Programming Homework 3: If Statements
+
+Objective:
+Write a program that asks for a password.
+If the password is correct, output "Correct Password!"; 
+if the password isn't correct output "Invalid Password!".
+
+Use the password: "cookies" 
+*/
+
+import java.util.Scanner
+
+public class If
+{
+   public static void main(String[] args)
+   {
+   
+   
+   
+   }
+}
---
+++ homework/Input.java
@@ -0,0 +1,20 @@
+/* 
+Programming Homework 2: Input
+
+Objective:
+Write a program that takes two numbers from user input
+then outputs the product of both numbers.
+
+*/
+
+import java.util.Scanner
+
+public class Input
+{
+   public static void main (String[] args)
+   {
+   
+   
+   
+   }
+}
