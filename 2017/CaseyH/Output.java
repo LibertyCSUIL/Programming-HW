bd56cb58fb3272cf30e61afc2a3859df4dbf43a5
Casey <user@Users-Computer-59.local>
2017-09-28 17:27:58 -0500

    Output,java, 9/28/17 5:27PM

---
+++ 2017/CaseyH/Output.java
@@ -0,0 +1,14 @@
+/* 
+Programming Homework 1: Output
+
+Objective:
+Write a program that prints out the phrase "Hello, world!".
+*/
+
+public class Output
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+   
+    }
+}
