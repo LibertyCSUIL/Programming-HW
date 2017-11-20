a7d172f35a9e6432066e6ba7d5cb6b0804466b45
GavinDes <gavin17.business@gmail.com>
2017-11-19 18:21:04 -0600

    Fixed input.java

--- homework/Input.java
+++ homework/Input.java
@@ -7,9 +7,11 @@ Objective:
 Write a program that takes two numbers from user input
 then outputs the product of both numbers.
 
+Requirements:
+Values must be doubles
 */
 
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Input
 {
